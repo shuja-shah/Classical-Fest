adba78610170d11a8626c1126f2c651861ddacac
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-07-25 13:32:55 -0700

    Develop Style for Programe

---
+++ Scripts/navBar.js
@@ -0,0 +1,14 @@
+const navi = document.getElementById("hamburger");
+const head = document.querySelector("header");
+const main = document.getElementById("man");
+const x = document.getElementById("btn-close");
+const navigation = document.getElementById("nav-bar");
+const links = document.getElementById("navBarr");
+const section = document.getElementById("home");
+// navi.addEventListener("click", () => {
+//   main.style.opacity = "0";
+//   navigation.style.display = "block";
+//   links.style.display = "flex";
+//   head.style.display = "flex";
+//   section.style.opacity = "0";
+// });
