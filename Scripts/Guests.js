a1222d08007ac7b885c093d49b2eabf7409d5485
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-07-26 02:21:33 -0700

    Define Array

---
+++ Scripts/Guests.js
@@ -0,0 +1,14 @@
+const artist = [
+    { name: 'Abida Parveen', id: '0', description :'Abida Parveen is a sufi singer from Sindh, She Specalizes in the art of'
++ ' Sufism and she devotes her life to the spirituality of the world.' , img : 'Assets/AbidaP.png'},
+    { name: 'Deva Premal', id: '1', description :'Deva Premal is a singer known for introducing Sanskrit mantras into the mainstream'
+    + 'Her meditative, spiritual music produced with Prabhu Music puts ancient Tibetan and Sanskrit mantras into contemporary settings.', img : 'Assets/Dava.jfif'},
+    { name :'Roger Waters', id: '2', description :'George Roger Waters is an English musician, singer songwriter and composer'
+     + ' who is known for his work in the genre of Spritual music.', img : 'Assets/Rogers.jpg'},
+    { name :'Ludovico Einaudi', id: '3', description :'Ludovico Maria Enrico Einaudi OMRI is an Italian pianist and composer. Trained at the Conservatorio Verdi in Milan'
+    + 'Einaudi began his career as a classical composer, later incorporating other styles and genres such as pop, rock, folk, and world music', img :'Assets/Ludovico.jfif'},
+    {name: 'Batzorig Vaanchig', id: '5', description: 'Batzorig Vaanchig Is an Mongolion Thorat Singer who specializes in igniting'
+    + ' the fire of the great Mongol Historien Leader Gengis Khan.', img : 'Assets/Batzorig.jfif'},
+    {name : 'Andre Watts', id: '6', description: 'Andre Watts is an American singer, songwriter, and musician. He is known for his'
+    + ' work in the genre of Classical Music.', img : 'Assets/Andre.jfif'}
+];
